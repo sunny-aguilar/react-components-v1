9afdec35abfbec750d0a9fe5cff89f176d32e8dd
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-11 02:12:24 -0700

    components

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -3,7 +3,8 @@ import React from "react";
 import faker from "faker";
 
 // Create a react component
-const CommentDetail = () => {
+const CommentDetail = (props) => {
+  console.log(props);
   return (
     <div className="comment">
       <a href="/" className="avatar">
@@ -11,7 +12,7 @@ const CommentDetail = () => {
       </a>
       <div className="content">
         <a href="/" className="author">
-          {faker.name.firstName()}
+          {props.author}
         </a>
         <div className="metadata">
           <span className="date">
--- src/index.js
+++ src/index.js
@@ -9,12 +9,12 @@ const App = () => {
   return (
     <div className="ui container comments">
       {/* JSX components are treated as tags */}
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail author={faker.name.firstName()} />
+      <CommentDetail author={faker.name.firstName()} />
+      <CommentDetail author={faker.name.firstName()} />
     </div>
   );
 };
-
+// {faker.name.firstName()}
 // Take the react component and show it on the screen
 ReactDOM.render(<App />, document.querySelector("#root"));
